98b09411bd0b476c4fe9b2b68993d3c5df8e6bc3
qishichang <qishichang@live.com>
2019-09-25 20:48:00 +1200

    Add barrel for product state

--- src/app/products/containers/product-shell/product-shell.component.ts
+++ src/app/products/containers/product-shell/product-shell.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 
 /* NgRx */
 import { Store, select } from '@ngrx/store';
-import * as fromProduct from '../../state/product.reducer';
+import * as fromProduct from '../../state/';
 import * as productActions from '../../state/product.action';
 import { Product } from '../../product';
 
